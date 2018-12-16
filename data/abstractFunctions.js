67522607ff64dad08ac7ba61674f5df771f212aa
Manish Motwani <mmotwani@cs.umass.edu>
2018-12-16 17:44:28 -0500

    manually written abstract functions used in the generated test files
    Former-commit-id: 90f422af9ad7cacc4fc6790d6377ccd9c45cb72f

---
+++ data/abstractFunctions.js
@@ -0,0 +1,91 @@
+/*
+*
+*	ABSTRACT FUNCTIONS IMPELEMTED USING DESCRIPTION FROM ECMA-262 
+*
+*/
+
+function RequireObjectCoercible(argument){
+        if (typeof argument == "undefined" || typeof argument == "null")
+                throw new TypeError("TypeError in RequireObjectCoercible")
+        else
+                return argument
+}
+
+function ToString(argument){
+        if (typeof argument == "undefined")
+                return "undefined"
+        else if(typeof argument == "null")
+                return "null"
+        else if(typeof argument == "boolean"){
+                if (argument == true)
+                        return "true"
+                else
+                        return "false"
+
+        }
+        else if(typeof argument == "string")
+                return argument
+        else if(typeof argument == "number")
+                return Number(argument).toString()
+        else if(typeof argument == "symbol")
+                throw new TypeError("TypeError in ToString")
+
+
+}
+
+function ToInteger(argument){
+        var number = Number(argument)
+        if (Object.is(number, NaN)){
+                return 0
+        }
+        if (number == 0 || number == Infinity || number == -Infinity || number == +0 || number== -0){
+                return number
+        }
+        return number-(number%1)
+}
+
+function ToUint32(argument){
+        var number = Number(argument)
+        if (Object.is(number, NaN) || number == 0 || number == Infinity || number == -Infinity || number == +0 || number== -0){
+                return 0
+        }
+        var i = Math.floor(Math.abs(number))
+        var int32bit = i%(Math.pow(2,32))
+    return int32bit
+}
+
+function IsRegExp(argument){
+        return (argument instanceof RegExp)
+}
+
+function IsCallable(argument){
+        if (typeof argument != 'object')
+                return false
+        if (typeof argument == 'function')
+                return true
+        return false
+}
+
+function ToLength(argument){
+        var len = ToInteger(argument);
+        if (len <= 0)
+                return 0
+        return Math.min(len, 2e53-1)
+}
+
+function ToObject(argument){
+        if (typeof argument == 'undefined' || typeof argument == 'null')
+                throw new TypeError("TypeError in ToObject")
+        if (typeof argument == "number")
+                return new Number(argument)
+        if (typeof argument == "string")
+                return new String(argument)
+        if (typeof argument == "boolean")
+                return new Boolean(argument)
+        if (typeof argument == "symbol")
+                return new Symbol(argument)
+        if (typeof argument == "object")
+                return argument
+}
+
+
