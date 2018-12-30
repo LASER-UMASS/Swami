59c3981395ed57740d3007efed283d2ba1593345
Manish Motwani <mmotwani@cs.umass.edu>
2018-12-30 02:30:04 -0500

    script to execute everything for RQ1 and RQ2
    Former-commit-id: ebf3aca03d333a560e54855be6f46ed4060be1a7

--- src/generateTestsFromScratch.sh
+++ src/generateTestsFromScratch.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
 echo "STEP1: Extract Relevant sections"
+echo
 rm -rf ../results//Node_ECMA262_Tests/
-python swami.py ../data/ECMA-262_v8.txt ../data/abstractFunctions.js ../results/ getRelSections node 1000
+python3 swami.py ../data/ECMA-262_v8.txt ../data/abstractFunctions.js ../results/ getRelSections node 1000
 
-echo "STEP2: Generate Templates for extracted Relevant sections"
+echo
+echo "STEP2: Generate Templates for extracted Relevant sections Node.js"
+echo
 rm -rf ../results//Node_ECMA262_Tests/
-python swami.py ../data/ECMA-262_v8.txt ../data/abstractFunctions.js ../results/ genTemplates node 1000
+python3 swami.py ../data/ECMA-262_v8.txt ../data/abstractFunctions.js ../results/ genTemplates node 1000
 
-echo "STEP3: Instantiate generated Templates"
-rm -rf ../results//Node_ECMA262_Tests/
-python swami.py ../data/ECMA-262_v8.txt ../data/abstractFunctions.js ../results/ genTests node 1000
+echo
+echo "STEP3: Instantiate generated Templates for Node.js"
+echo
+rm -rf ../results/Node_ECMA262_Tests/
+python3 swami.py ../data/ECMA-262_v8.txt ../data/abstractFunctions.js ../results/ genTests node 1000
 
-echo "STEP4: Run and analyse generated Tests"
+echo
+echo "STEP4: Run and analyse generated Tests on Node.js"
+echo
 ./analyzeGeneratedTests.sh
 
+echo
+echo "STEP5: Generate Templates for extracted Relevant sections for Rhino"
+echo
+rm -rf ../results/Rhino_ECMA262_Tests/
+python3 swami.py ../data/ECMA-262_v8.txt ../data/abstractFunctions.js ../results/ genTemplates rhino 1000
+
+echo
+echo "STEP6: Instantiate generated Templates for Rhino"
+echo
+rm -rf ../results/Rhino_ECMA262_Tests/
+python3 swami.py ../data/ECMA-262_v8.txt ../data/abstractFunctions.js ../results/ genTests rhino 1000
+
+echo
+echo "STEP7: Run and analyse generated Tests on Rhino"
+echo
+./runTestsInRhino.sh
