3f8261029f7353fee3eea158a7959845a3571bf6
Manish Motwani <mmotwani@cs.umass.edu>
2019-01-07 03:46:52 -0500

    script to replicate results for RQ1
    Former-commit-id: ce0419c6309555a0ada6d5a9add4ae264eaf8ad3

---
+++ src/reproduceRQ1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "STEP1: Extract Relevant sections"
+echo
+rm -rf ../results//Node_ECMA262_Tests/
+python3 swami.py ../data/ECMA-262_v8.txt ../data/abstractFunctions.js ../results/ getRelSections node 1000
+
+echo
+echo "STEP2: Generate Templates for extracted Relevant sections Node.js"
+echo
+rm -rf ../results//Node_ECMA262_Tests/
+python3 swami.py ../data/ECMA-262_v8.txt ../data/abstractFunctions.js ../results/ genTemplates node 1000
+
+echo
+echo "STEP3: Instantiate generated Templates for Node.js"
+echo
+rm -rf ../results/Node_ECMA262_Tests/
+python3 swami.py ../data/ECMA-262_v8.txt ../data/abstractFunctions.js ../results/ genTests node 1000
+
+echo
+echo "STEP4: Run and analyse generated Tests on Node.js"
+echo
+./analyzeGeneratedTests.sh
+
