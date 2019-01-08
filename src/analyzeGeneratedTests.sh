01795b8ac55b05b3db6725d33397955dbe27db05
Manish Motwani <mmotwani@cs.umass.edu>
2019-01-07 23:15:03 -0500

    using correct version of node, making results more explicit
    Former-commit-id: b70e6392a5be64daf9b3504c3b7e50fe754f4892

--- src/analyzeGeneratedTests.sh
+++ src/analyzeGeneratedTests.sh
@@ -4,7 +4,7 @@ if [ -f rq1_output.txt ]; then
     rm rq1_output.txt
 fi
 
-for i in ../results/Node_ECMA262_Tests/*.js;do echo $i; node $i; done &>> rq1_output.txt;
+for i in ../results/Node_ECMA262_Tests/*.js;do echo $i; ~/node-10.7.0/node $i; done &>> rq1_output.txt;
 
 echo
 echo "Total #tests generated: $(grep " Test" rq1_output.txt | wc -l)"
@@ -14,10 +14,10 @@ echo "#OK tests: $(grep "OK Test" rq1_output.txt | wc -l)"
 echo
 
 #echo "Good Tests per test file"
-#for i in ../results/Node_ECMA262_Tests/*.js;do echo $i; ~/Desktop/node-10.7.0/node $i | grep "Good Test" | wc -l; done;
+#for i in ../results/Node_ECMA262_Tests/*.js;do echo $i; ~/node-10.7.0/node $i | grep "Good Test" | wc -l; done;
 
 #echo "Bad Tests per test file"
-#for i in ../results/Node_ECMA262_Tests/*.js;do echo $i; ~/Desktop/node-10.7.0/node $i | grep "Bad Test" | wc -l; done;
+#for i in ../results/Node_ECMA262_Tests/*.js;do echo $i; ~/node-10.7.0/node $i | grep "Bad Test" | wc -l; done;
 
 echo
 echo "Manual analysis of 998 Bad/Failing tests of test-get-arraybuffer-prototype-bytelength.js test file reveal that these are actually failing tests (hence Good tests) while remaining ones are false alarms. Thus, the revised number of Good, Bad, Innocuous, and Non-innocuous tests is:"
@@ -29,10 +29,10 @@ innocuous=$(( 83000 - $noninnocuous ))
 echo
 echo "######### REPLICATING RESULTS REPORTED FOR RQ1 ###############"
 echo
-echo "#Good tests: $good"
-echo "#Bad tests: $bad"
-echo "#Non-innocuous tests: $noninnocuous"
-echo "#Innocuous tests: $innocuous"
+echo "#Good tests= 31381 + 998 =  $good"
+echo "#Bad tests: 1533 - 998 = $bad"
+echo "#Non-innocuous tests: 31381 + 1533 = $noninnocuous"
+echo "#Innocuous tests: 83000 - 31381 - 1533 = $innocuous"
 echo "Percent of Good tests (out of non-innocous): $(bc <<< "scale=2; 3237900/32914")%"
 echo "Percent of Bad tests (out of non-innocous): $(bc <<< "scale=2; 53500/32914")%"
 echo "Percent of Good tests (out of total): $(bc <<< "scale=2; 3237900/83000")%"
