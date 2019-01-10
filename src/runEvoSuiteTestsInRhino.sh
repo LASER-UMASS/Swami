9744fa173a6018660ee4ff27378b6737dc1d795c
Manish Motwani <mmotwani@cs.umass.edu>
2019-01-10 04:05:16 -0500

    scripts to replicate results for RQ3
    Former-commit-id: f620813c6a9e004a484f94b000716fe641e862d8

---
+++ src/runEvoSuiteTestsInRhino.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+cd ~/rhino-Rhino1_7_9_Release/
+
+if [ -d buildEvoSuite1 ]; then
+   rm -rf buildEvoSuite1
+fi
+
+if [ -d buildEvoSuite2 ]; then
+   rm -rf buildEvoSuite2
+fi
+
+if [ -d buildEvoSuite3 ]; then
+   rm -rf buildEvoSuite3
+fi
+
+if [ -d buildEvoSuite4 ]; then
+   rm -rf buildEvoSuite4
+fi
+
+if [ -d buildEvoSuite5 ]; then
+   rm -rf buildEvoSuite5
+fi
+
+if [ -d buildEvoSuitePlusSwami ]; then
+   rm -rf buildEvoSuitePlusSwami
+fi
+
+echo "RHINO: EXECUTING EVOSUITE TEST SUITE#1 ................................................................................."
+cd ~/rhino-Rhino1_7_9_Release/
+cp build.gradle_es1 build.gradle
+./gradlew clean
+./gradlew jar
+./gradlew test
+mv buildGradle buildEvoSuite1
+
+echo "RHINO: EXECUTING EVOSUITE TEST SUITE#2 ................................................................................."
+cd ~/rhino-Rhino1_7_9_Release/
+cp build.gradle_es2 build.gradle
+./gradlew clean
+./gradlew jar
+./gradlew test
+mv buildGradle buildEvoSuite2
+
+echo "RHINO: EXECUTING EVOSUITE TEST SUITE#3 ................................................................................."
+cd ~/rhino-Rhino1_7_9_Release/
+cp build.gradle_es3 build.gradle
+./gradlew clean
+./gradlew jar
+./gradlew test
+mv buildGradle buildEvoSuite3
+
+echo "RHINO: EXECUTING EVOSUITE TEST SUITE#4 ................................................................................."
+cd ~/rhino-Rhino1_7_9_Release/
+cp build.gradle_es4 build.gradle
+./gradlew clean
+./gradlew jar
+./gradlew test
+mv buildGradle buildEvoSuite4
+
+echo "RHINO: EXECUTING EVOSUITE TEST SUITE#1 ................................................................................."
+cd ~/rhino-Rhino1_7_9_Release/
+cp build.gradle_es5 build.gradle
+./gradlew clean
+./gradlew jar
+./gradlew test
+mv buildGradle buildEvoSuite5
