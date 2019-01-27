5de328f94935e4c78b3c8f6525d7427dcb5f853b
Manish Motwani <mmotwani@cs.umass.edu>
2019-01-27 16:03:51 -0500

    making it easier for the reviewer to run the script
    Former-commit-id: 3434d4b3823f438883737b3e83c69e3a28a3a3a7

---
+++ src/coverageAnalysisEvoSuite.sh
@@ -0,0 +1,6 @@
+echo
+echo "Compute statement-coverage achieved by EvoSuite-generated tests"
+echo
+python coverageAnalysisEvoSuite.py ../EvoSuiteTests/evosuite_1/evosuite-report/statistics.csv ../EvoSuiteTests/evosuite_2/evosuite-report/statistics.csv ../EvoSuiteTests/evosuite_3/evosuite-report/statistics.csv ../EvoSuiteTests/evosuite_4/evosuite-report/statistics.csv ../EvoSuiteTests/evosuite_5/evosuite-report/statistics.csv
+
+
